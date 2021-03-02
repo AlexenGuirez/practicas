1dbb6161c60a9763edc6a35cf070a862b08778f6
Guirez <46309665+AlexenGuirez@users.noreply.github.com>
2021-03-01 16:09:22 -0800

    Update meta 1_4.cpp

--- c++/meta 1_4.cpp
+++ c++/meta 1_4.cpp
@@ -23,19 +23,24 @@ int div(int a,int b){
 		
 main(){
 	int a,b,exit,operacion,resultado; 
-	cout <<"Elige que operacion vas hacer" <<endl << "tienes de opcion: suma(1),resta(2),multi(3) y div(4)..." << endl;	cin >> operacion;  
-	cout << "Elige el primer valor: ";   cin >> a;   
-	cout << "Elige el segundo valor: " ; cin >> b; 
+	cout <<"Elige que operacion vas hacer" <<endl << "tienes 4 opciones: " << endl;
+	cout <<"Suma elige 1, Resta elige 2, Multiplicacion elige 3, division elige 4"<<endl; cin >> operacion; 
+	cout <<"Inserta tus 2 valores"<<endl; 
+	cout << "primer valor: ";   cin >> a;   
+	cout << "segundo valor: " ; cin >> b; 
 	switch(operacion) {
 		case 1: resultado = suma(a,b);
-		cout << "El resultado de la suma fue: "    << resultado << endl;break;
+		cout << "El resultado de la suma fue: "    << resultado << endl;
+		break;
 		case 2: resultado = resta(a,b);
-		cout << "El resultado de la resta fue: "   << resultado << endl;break;
+		cout << "El resultado de la resta fue: "   << resultado << endl;
+		break;
 		case 3: resultado = multi(a,b);
-		cout << "El resultado de la multi fue: "   << resultado << endl;break;
+		cout << "El resultado de la multi fue: "   << resultado << endl;
+		break;
 		case 4: resultado = div(a,b);
-		cout << "El resultado de la division fue: "<< resultado << endl;break;
-		default: cout << "seleccionaste una operacion que no existe";
+		cout << "El resultado de la division fue: "<< resultado << endl;
+		break;
+		default: cout << "Error, no existe esa Operacion";
     }
-    cin >> exit;
 }
